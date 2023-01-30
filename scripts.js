3bc130e5c25d7f673c313b5d07cf559c3e00c4d7
Tyler <tylerm180@gmail.com>
2023-01-30 13:25:46 -0500

    Final touches to get to final working product

--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@
 // find the container div
 const container = document.querySelector("#container");
 
+function sizePrompt(){ 
+    const returnSize = prompt("Enter size: ");
+    return returnSize;
+}
 
 
 function addHoverClass(e) {
@@ -12,42 +16,50 @@ function removeHoverClass(e){
     e.target.classList.remove("hovered");
 }
 
-/* function reSize(){
-    const newSize = Number(prompt("Enter size: "));
-    console.log(newSize);
-    deleteBoxes();
-    for (let i = 0; i < newSize; i++){
-        const newDiv = document.createElement(`div`);
-        container.appendChild(newDiv);
-        newDiv.setAttribute("class", `class${i}`);
-        newDiv.setAttribute("value", `${i}`);
-    }
-    return newSize;
-}
-*/
-
-
-// generate 16x16 boxes for inital plot
-for (let a = 1; a < 17; a++){
-    const newCol = document.createElement('div'); 
-    container.appendChild(newCol);
-    newCol.setAttribute('id', `rowbox`);
-    for (let i = 1; i < 17; i++){
-        const newRow = document.createElement(`div`);
-        newCol.appendChild(newRow);
-        newRow.setAttribute("class", `colbox`);
-    }
-}
-
-//select divs within each
-const divs = container.querySelectorAll("div");
-console.log(divs.length);
-
 function deleteBoxes() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 }
+
+function grid(size) {
+    deleteBoxes();
+    for (let a = 0; a < size; a++){
+        const newCol = document.createElement('div'); 
+        container.appendChild(newCol);
+        newCol.setAttribute('id', `rowbox`);
+        for (let i = 0; i < size; i++){
+            const newRow = document.createElement(`div`);
+            newCol.appendChild(newRow);
+            newRow.setAttribute("class", `colbox`);
+        }
+    }
+    const squares = container.querySelectorAll("div#rowbox > div");
+    console.log(squares.length);
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", addHoverClass)
+    //square.addEventListener("mouseleave", removeHoverClass)
+    });  
+}
+
+
+// button function to let user enter grid size and pass it as a function to grid generating function
+function newGrid() {
+    let gridSize = sizePrompt();
+    if (gridSize > 100){
+        gridSize = sizePrompt();
+    }
+    grid(gridSize);
+}
+
+//set initial grid
+grid(16);
+
+
+//select divs within each
+const divs = container.querySelectorAll("div");
+console.log(divs.length);
+
 
 
 // select divs and add event listeners for all of them
@@ -55,23 +67,5 @@ const squares = container.querySelectorAll("div#rowbox > div");
 console.log(squares.length);
 squares.forEach(square => {
     square.addEventListener("mouseenter", addHoverClass)
-    square.addEventListener("mouseleave", removeHoverClass)
+    //square.addEventListener("mouseleave", removeHoverClass)
 });
-
-
-
-/*function makeGrid() {
-    deleteBoxes();
-    const size = prompt("Enter size: ");
-    for (let i = 0; i < size; i++){
-        const newRow = document.createElement("div"); 
-        container.appendChild(newRow);
-        newRow.setAttribute('id', `rowbox`);
-        for (let x = 0; x < size; x++){
-            const newColumn = document.createElement("div");
-            newRow.appendChild(newColumn);
-            newColumn.setAttribute('id', `colbox`);
-        }
-    }
-}
-*/ 
